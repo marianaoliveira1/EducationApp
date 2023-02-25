551cb85bd9bee96023816819c152f721777c71b9
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-25 12:57:03 -0300

    adicionado as imagens e parte da welcome

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +7,34 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF674AEF),
+                      borderRadius:
+                          BorderRadius.only(bottomRight: Radius.circular(70))),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
