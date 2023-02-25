c916665ed1d717d2afce16d686477c595792344a
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-25 13:11:12 -0300

    welcome

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -29,9 +29,35 @@ class WelcomeScreen extends StatelessWidget {
                       color: Color(0xFF674AEF),
                       borderRadius:
                           BorderRadius.only(bottomRight: Radius.circular(70))),
+                  child: Center(
+                      child: Image.asset(
+                    "images/books.png",
+                    scale: 0.8,
+                  )),
                 ),
               ],
-            )
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                decoration: BoxDecoration(
+                  color: Color(0xFF674AEF),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(70))),
+              ),
+            ),
           ],
         ),
       ),
