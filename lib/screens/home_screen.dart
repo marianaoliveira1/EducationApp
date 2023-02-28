76cb7a88bdbbd153cdade51735d25a415de8288c
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-28 19:13:13 -0300

    imagens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -53,6 +55,8 @@ class HomeScreen extends StatelessWidget {
       size: 30,
     ),
   ];
+
+  List imgList = ['Flutter', 'React Native', 'Python', 'C#'];
 
   @override
   Widget build(BuildContext context) {
@@ -142,8 +146,76 @@ class HomeScreen extends StatelessWidget {
                           decoration: BoxDecoration(
                               color: catColors[index], shape: BoxShape.circle),
                           child: Center(child: catIcons[index]),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          catNames[index],
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black.withOpacity(.7)),
                         )
                       ],
+                    );
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Courses",
+                      style:
+                          TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF674AEF)),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                GridView.builder(
+                  itemCount: imgList.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio:
+                          (MediaQuery.of(context).size.height - 50 - 25),
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xFFF5F3FF)),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                "images/${imgList[index]}.png",
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            )
+                          ],
+                        ),
+                      ),
                     );
                   },
                 )
